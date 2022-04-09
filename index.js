1699f904ad5a7c3f603934e0e05e54000957c613
Yuki Shimada <emadurandal@gmail.com>
2022-04-09 19:51:00 +0900

    add code

---
+++ index.js
@@ -0,0 +1,34 @@
+const requireFile = require("shaderity-node").requireFile;
+const getShaderStage = require("shaderity-node").shaderStage;
+const path = require('path');
+const fs = require('fs');
+module.exports = options => {
+  return {
+    name: 'shaderity',
+    setup (build) {
+      build.onResolve({ filter: /\.(glsl|vs|fs|vert|frag)$/ }, (args) => {
+        return {
+          path: path.resolve(args.resolveDir, args.path),
+          namespace: 'shaderity'
+        }
+      });
+      build.onLoad({ filter: /.*/, namespace: 'shaderity' }, (args) => {
+        const source = fs.readFileSync(args.path, 'utf-8')
+        const code = requireFile(source, args.path)
+        const shaderStage = getShaderStage(args.path)
+        const isFragmentShader = shaderStage === 'fragment'
+
+        const json = {
+          code,
+          shaderStage,
+          isFragmentShader,
+        };
+
+        return {
+          contents: json,
+          loader: 'json'
+        }
+      });
+    }
+  }
+}
